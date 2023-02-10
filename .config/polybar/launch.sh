8ea209a393b11da0fbc2f80314fe952b70318691
Andres Copeticona <andrescopeticona@outlook.es>
2023-02-10 11:11:54 -0400

    add tray icons in polybar

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -8,6 +8,7 @@ killall -q polybar
 # Launch Polybar, using default config location ~/.config/polybar/config.ini
 polybar primary -r 2>&1 | tee -a /tmp/polybar.log & disown
 polybar leftbar -r 2>&1 | tee -a /tmp/polybar.log & disown
+polybar traybar -r 2>&1 | tee -a /tmp/polybar.log & disown
 polybar rightbar -r 2>&1 | tee -a /tmp/polybar.log & disown
 
 echo "Polybar launched..."
