6b25beab705cdd6cb481b30bdb6faa58949fd328
Andres Copeticona <andrescopeticona@outlook.es>
2023-07-14 12:06:44 -0400

    change zhs confign and lazyvim

--- .config/qtile/autostart.sh
+++ .config/qtile/autostart.sh
@@ -2,5 +2,7 @@
 
 setxkbmap -layout us -variant altgr-intl # change keyboard layout
 feh --bg-fill /home/zero/.dotfiles/wallpapers/wallpaper10.jpg &
+nm-applet &
+volumeicon &
 picom &
 udiskie -t &
