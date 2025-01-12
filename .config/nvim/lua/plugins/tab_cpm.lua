adb45620ec6f792495d1816a529a138683933b86
Andres Copeticona <andrescopeticona@outlook.es>
2025-01-12 19:33:19 -0400

    update blink conf

--- .config/nvim/lua/plugins/tab_cpm.lua
+++ .config/nvim/lua/plugins/tab_cpm.lua
@@ -6,12 +6,24 @@ return {
     },
     event = { "LspAttach" },
     opts = {
+      completion = {
+        list = {
+          selection = {
+            preselect = true,
+            auto_insert = false,
+          },
+        },
+      },
       keymap = {
         preset = "default",
         ["<C-e>"] = { "hide" },
-        ["<Tab>"] = { "select_and_accept", "fallback" },
+        ["<Tab>"] = { "snippet_forward", "select_and_accept", "fallback" },
+        ["<S-Tab>"] = { "snippet_backward", "fallback" },
         ["<C-k>"] = { "select_prev", "fallback" },
         ["<C-j>"] = { "select_next", "fallback" },
+        ["<C-p>"] = {},
+        ["<C-n>"] = {},
+        ["<C-y>"] = {},
       },
     },
   },
